4835cca93f76283d94040dae0013f58e2a25b009
sooraj <tsooraj111@gmail.com>
2023-10-24 13:57:14 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import TodoApp from './component/TodoApp';
+import About from './component/About/About';
+import Header from './component/Header/Header';
+import TodoApp from './component/TodoApp/TodoApp';
 import React,{Component} from "react";
+import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <TodoApp/>
+        <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<TodoApp/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+        </Routes>
+        </Router>
       </div>
     )
   }
---
+++ src/component/About/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./About.css"
+export default function About() {
+  return (
+    <div className='about'>
+        <p>This is a ToDo Application, which is used to make lists of things to do for you.</p>
+    </div>
+  )
+}
